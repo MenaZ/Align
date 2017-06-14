81bd33f386da58516b26bfeb1413412eadee908b
Youssef Ouirini <Youssef Ouirini>
2017-06-14 11:15:20 +0200

    setted up database tables

--- app.js
+++ app.js
@@ -20,3 +20,41 @@ app.use('/', bodyParser());
 app.set('views', './');
 app.set('view engine', 'pug');
 app.use(express.static("public"));
+
+// Setting up the tables
+var User = sequelize.define('user', {
+	firstname: Sequelize.STRING,
+	lastname: Sequelize.STRING,
+	email: Sequelize.STRING,
+	age: Sequelize.INTEGER,
+	gender: Sequelize.STRING,
+	password: Sequelize.STRING,
+	aboutme: Sequelize.STRING
+});
+
+var Event= sequelize.define('events', {
+	title: Sequelize.STRING,
+	description: Sequelize.STRING,
+	date: Sequelize.STRING
+})
+
+var Comment= sequelize.define('comment', {
+	body: Sequelize.STRING
+})
+
+// Setting up the model by linking the tables to each other
+Event.belongsTo(User);
+User.hasMany(Event);
+User.hasMany(Comment);
+Comment.belongsTo(User);
+Event.hasMany(Comment);
+Comment.belongsTo(Event);
+
+sequelize.sync({force: false}) //Change false to true to wipe clean the whole database.
+
+// Creates session when user logs in
+app.use(session({
+	secret: `#{process.env.SECRET_SESSION}`,
+	resave: true,
+	saveUninitialized: false
+}));
